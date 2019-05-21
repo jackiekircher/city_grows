eb095b7dc1d929c27b1f11515a0c0bce9f9a3fab
jackie kircher <kircher.jackie@gmail.com>
2019-05-21 15:15:34 -0400

    [WIP] define plant growth methods
    leaving some pending specs to describe the functionality we expect out
    of a plant

--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Plant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  Given(:subject) do
+    described_class.new
+  end
+
+  describe '#has_water?' do
+    pending 'should be based off of the plant\'s water_interval and how recently the pot was watered'
+  end
+
+  describe '#has_sunlight?' do
+    pending 'should be based off of how much sunlight the plant\'s pot has and if it is appropraite for the plant type'
+  end
+
+  describe '#grow' do
+    pending 'if both water and sunlight conditions are met, the plant should grow by 1!'
+  end
 end
